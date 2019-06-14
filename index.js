a5c98184afcfb1122190f4c75486b23368f4b1b1
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 19:09:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ class Trip {
     this.name = name
     this.driverId = driverId++
     this.passengerId = passengerId++
-    this.id = trip.id++
+    this.id = tripId++
     store.trips.push(this)
   }
   
